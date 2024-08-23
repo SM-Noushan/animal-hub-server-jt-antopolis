bb07725d86285aaecdccfd5b0521ed5f2aa12aac
SMNoushan <noushan.md@gmail.com>
2024-08-23 06:59:42 +0600

    updated: cors origin

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 8000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://animalhub-by-sm-nowshan.vercel.app",
+    ],
   })
 );
 
